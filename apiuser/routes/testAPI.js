ba9ad55b6f0528472b12e1a14a87a0bcb92eae4b
ANH48 <minhnhut.nguyen@hiq.ai>
2021-09-10 12:01:52 +0700

    first commit

---
+++ apiuser/routes/testAPI.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const newsRouter = express.Router();
+
+newsRouter.get('/',(req,res,next) => {
+  res.send("API is working")
+  })
+
+module.exports = newsRouter 
